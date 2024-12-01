b11818a60226966ead86e106e22ddc2b6d127ab9
pat2echo <pat2echo@gmail.com>
2024-12-01 19:07:59 +0100

    Label Validation Video 4

--- proof-of-concept/pose-estimation-on-video/analyze_manual_label.py
+++ proof-of-concept/pose-estimation-on-video/analyze_manual_label.py
@@ -7,7 +7,7 @@ if len(sys.argv) > 1:
     print(sys.argv[1])
     if len(sys.argv) > 2 and int(sys.argv[2]) > 0:
         show_all_classes = True
-    plot_activity_fall(csv_file=sys.argv[1], plot_title=os.path.basename(sys.argv[1]).split('.')[0], show_all_classes=show_all_classes)
+    plot_activity_fall(csv_file=sys.argv[1], plot_title=f'analyze_label_{os.path.basename(sys.argv[1]).split('.')[0]}', show_all_classes=show_all_classes)
 else:
     print("Expecting csv file name of manual labels")
     exit()
